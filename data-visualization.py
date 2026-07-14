94d046858e3ffaff5bf5d208b4bae140d8e779ce
anjulanadeeshan <mranjula660@gmail.com>
2026-07-14 12:12:40 +0530

    Refactor data visualization code; comment out unused plots and add correlation heatmap

--- data-visualization.py
+++ data-visualization.py
@@ -6,17 +6,26 @@ df = pd.read_csv('./student_data_cleaned.csv')
 print('cleaned dataset')
 print(df)
 
-plt.figure(figsize=(6, 5))
-#counts how many males and females are in the dataset
-gender_count = df['Gender'].value_counts()
-#create bar charts
-plt.bar(gender_count.index, gender_count.values, color=['skyblue','pink'])
-plt.title('Bar Chart - Count of Gender')
-plt.xlabel('Gender')
-plt.ylabel('Count')
-plt.show()
+# plt.figure(figsize=(6, 5))
+# #counts how many males and females are in the dataset
+# gender_count = df['Gender'].value_counts()
+# #create bar charts
+# plt.bar(gender_count.index, gender_count.values, color=['skyblue','pink'])
+# plt.title('Bar Chart - Count of Gender')
+# plt.xlabel('Gender')
+# plt.ylabel('Count')
+# plt.show()
+
+# plt.figure(figsize=(8,6))
+# #create scatter plot
+# plt.scatter(df['Age'], df['GPA'], color='purple')
+# plt.title('Scatter Plot - Age vs GPA')
+# plt.show()
 
 plt.figure(figsize=(8,6))
-plt.scatter(df['Age'], df['GPA'], color='purple')
-plt.title('Scatter Plot - Age vs GPA')
+# Correlation HeatMap
+
+corr = df[['Age', 'GPA', 'Attendance', 'Salary']].corr()
+sb.heatmap(corr, annot=True, cmap='coolwarm', center=0, linewidths=0.5)
+plt.title('heatmap (correlation)')
 plt.show()
